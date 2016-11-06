0e5eaacfa47948e90fde181e6e6225b8d3bb5ced
kranze <martin.kranzfelder@gmail.com>
2016-11-06 11:24:46 +0100

    first

---
+++ SolarBMS/Release/.ino.cpp.d
@@ -0,0 +1,201 @@
+.ino.cpp.o: ../.ino.cpp \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Arduino.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/binary.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WCharacter.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WString.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/HardwareSerial.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Stream.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Print.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Printable.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/USBAPI.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Arduino.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\variants\standard/pins_arduino.h \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\libraries\EEPROM/EEPROM.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/ArduinoJson.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/DynamicJsonBuffer.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/BlockJsonBuffer.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonBuffer.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Arduino/String.hpp \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WString.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariant.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonPrintable.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/../Configuration.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/DummyPrint.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/../Arduino/Print.hpp \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Print.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/IndentedPrint.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonWriter.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Encoding.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/ForceInline.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonFloat.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonInteger.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Prettyfier.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/StaticStringBuilder.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/DynamicStringBuilder.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonVariantContent.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonVariantType.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Unparsed.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariantBase.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonObjectKey.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/EnableIf.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsFloatingPoint.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsSame.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsIntegral.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/RemoveConst.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/RemoveReference.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariant.ipp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Parse.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArray.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/JsonBufferAllocated.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/List.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListConstIterator.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListNode.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListIterator.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ReferenceType.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/TypeTraits/IsReference.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArray.ipp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObject.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonPair.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObject.ipp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObjectSubscript.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArraySubscript.hpp \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/StaticJsonBuffer.hpp \
+ C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\libraries\SPI/SPI.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can_dfs.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can.h \
+ \\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can_dfs.h \
+ ../A123Module.h ../SolarBMS.ino
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Arduino.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/binary.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WCharacter.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WString.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/HardwareSerial.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Stream.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Print.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Printable.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/USBAPI.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Arduino.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\variants\standard/pins_arduino.h:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\libraries\EEPROM/EEPROM.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/ArduinoJson.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/DynamicJsonBuffer.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/BlockJsonBuffer.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonBuffer.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Arduino/String.hpp:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/WString.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariant.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonPrintable.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/../Configuration.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/DummyPrint.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/../Arduino/Print.hpp:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\cores\arduino/Print.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/IndentedPrint.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonWriter.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Encoding.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/ForceInline.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonFloat.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonInteger.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Prettyfier.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/StaticStringBuilder.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/DynamicStringBuilder.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonVariantContent.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/JsonVariantType.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Unparsed.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariantBase.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonObjectKey.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/EnableIf.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsFloatingPoint.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsSame.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/IsIntegral.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/RemoveConst.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../TypeTraits/RemoveReference.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../JsonVariant.ipp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/../Internals/Parse.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArray.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/JsonBufferAllocated.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/List.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListConstIterator.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListNode.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ListIterator.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/Internals/ReferenceType.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/TypeTraits/IsReference.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArray.ipp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObject.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonPair.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObject.ipp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonObjectSubscript.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/JsonArraySubscript.hpp:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\ArduinoJson/include/ArduinoJson/StaticJsonBuffer.hpp:
+
+C:\Users\kzm\eclipse\java-mars\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.7\libraries\SPI/SPI.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can_dfs.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can.h:
+
+\\ESG-GMBH.DE\home\kzm\Eigene\ Dateien\Arduino\libraries\MCP2515_lib-master/mcp_can_dfs.h:
+
+../A123Module.h:
+
+../SolarBMS.ino:
