c7f470d9c56ac890a7c9616747a66cf554e3ea1c
KZM <KZM@R5908.ESG-GMBH.DE>
2016-11-19 14:58:09 +0100

    with eclipse projekt

---
+++ SolarBMS/.ino.cpp
@@ -0,0 +1,23 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-11-19 14:57:37
+
+#include "Arduino.h"
+#include <EEPROM.h>
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <mcp_can_dfs.h>
+#include <mcp_can.h>
+#include "A123Module.h"
+void CAN_ISR() ;
+void setup() ;
+void loop();
+void step();
+void serialEvent() ;
+
+#include "SolarBMS.ino"
+
+
+#endif
